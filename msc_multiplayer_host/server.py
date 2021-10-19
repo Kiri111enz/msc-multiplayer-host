f4ee5a36a3995b4018837e152cb236a0f41c783d
Kiri111enz <omelyanenkokirill@gmail.com>
2021-10-19 19:33:35 +0700

    Implemented save transition from host to new clients.

--- msc_multiplayer_host/server.py
+++ msc_multiplayer_host/server.py
@@ -1,4 +1,4 @@
-from settings import SETTINGS, MESSAGE_ID
+from settings import SETTINGS, MESSAGE_ID, SAVE_ALERT
 from socket_utils import get_socket, try_recv
 import logger
 import socket as sk
@@ -12,9 +12,10 @@ class ThreadedServer:
         self._socket = get_socket()
         self._socket.bind(('', port))
 
-        self._pending_indexes = [0]
+        self._socket_by_index = {}
         self._nickname_by_index = {}
         self._to_send_by_index = {}
+        self._pending_indexes = [0]
 
     def start(self) -> None:
         logger.log('Server started.')
@@ -26,9 +27,9 @@ class ThreadedServer:
 
     def _handle_client(self, client: sk.socket) -> None:
         index = self._get_free_client_index()
-        logger.log(f'Client {index} connected.')
 
         try:
+            logger.log(f'Client {index} connected.')
             self._register_client(client, index)
             self._exchange_info_with_client(client, index)
         except ConnectionResetError:
@@ -37,13 +38,13 @@ class ThreadedServer:
             logger.log(repr(exception))
         finally:
             self._forget_client(client, index)
-
-        logger.log(f'Client {index} disconnected.')
+            logger.log(f'Client {index} disconnected.')
 
     def _register_client(self, client: sk.socket, index: int) -> None:
         nickname_size = try_recv(client, 1)
         nickname = nickname_size + try_recv(client, struct.unpack('b', nickname_size)[0])
         client.send(struct.pack('2b', index, len(self._nickname_by_index)))
+        self._socket_by_index[index] = client
 
         for client_index in self._to_send_by_index:
             self._to_send_by_index[client_index].put(struct.pack('2b', MESSAGE_ID.connected, index) + nickname)
@@ -55,6 +56,11 @@ class ThreadedServer:
     def _exchange_info_with_client(self, client: sk.socket, index: int) -> None:
         while True:
             message_size = try_recv(client, 1)
+
+            if message_size == SAVE_ALERT:
+                self._react_on_save_alert(index)
+                continue
+
             message = try_recv(client, struct.unpack('b', message_size)[0])
 
             for client_index in self._to_send_by_index:
@@ -68,6 +74,7 @@ class ThreadedServer:
         client.close()
 
         try:
+            del self._socket_by_index[index]
             del self._nickname_by_index[index]
             del self._to_send_by_index[index]
         except KeyError:
@@ -86,6 +93,33 @@ class ThreadedServer:
             self._pending_indexes.append(new_client_index + 1)
 
         return new_client_index
+
+    def _react_on_save_alert(self, index: int) -> None:
+        if index == 0:
+            self._send_save_to_new_client()
+            logger.log('Sent save to new client.')
+        else:
+            for client_index in self._socket_by_index:
+                if client_index != index:
+                    self._socket_by_index[index].send(SAVE_ALERT)
+
+            logger.log('New client loaded, told everyone to unpause.')
+
+    def _send_save_to_new_client(self) -> None:
+        host = self._socket_by_index[0]
+        new_client = self._socket_by_index[list(self._socket_by_index.keys())[-1]]
+
+        file_count = try_recv(host, 1)
+        new_client.send(file_count)
+
+        for i in range(struct.unpack('b', file_count)[0]):
+            file_size = struct.unpack('i', try_recv(host, 4))[0]
+
+            while file_size > 0:
+                chunk_size = min(8192, file_size)
+                chunk = try_recv(host, chunk_size)
+                new_client.send(chunk)
+                file_size -= len(chunk)
 
 
 if __name__ == '__main__':
--- msc_multiplayer_host/settings.py
+++ msc_multiplayer_host/settings.py
@@ -1,7 +1,10 @@
 from collections import namedtuple
+import struct
 
 _Settings = namedtuple('Settings', ('port', 'log_file_name', 'debug_mode'))
 SETTINGS = _Settings(25565, 'Logs/log_latest.txt', True)
 
 _MessageId = namedtuple('MessageId', ('connected', 'disconnected'))
 MESSAGE_ID = _MessageId(0, 1)
+
+SAVE_ALERT = struct.pack('b', 0)
